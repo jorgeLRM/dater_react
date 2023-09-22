5d0911377c02a732456d49d31c3e3768455bca70
jorgeLRM <jorge.lrm99@gmail.com>
2023-09-21 23:16:57 -0600

    Add new title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
             Follow-up
             <span className="text-indigo-600"> Veterinary </span>
-            Patients
+            Patients On Github
         </h1>
     )
 }
